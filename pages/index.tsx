93d5adce4bdc3d79564747f45f280fa84c83c5d8
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-09-08 13:12:56 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,31 +7,7 @@ function Home() {
       <section className="flex flex-col gap-6">
         <Text variant="h1">Loading Ads with the Script Component</Text>
         
-        <Text>
-          We have a loading state for ads while they load While our ads are
-          loading, we show a fallback state to reserve a space for them and
-          avoid a{' '}
-          <Link href="https://web.dev/cls/" target="_blank">
-            layout shift (CLS)
-          </Link>
-          .
-        </Text>
-
-        <Text>
-          The example introduces a voluntary 3 second lag in loading the ad and
-          it doesn&apos;t affect its{' '}
-          <Link
-            href="https://lighthouse-metrics.com/lighthouse/checks/b65fc6a3-323c-43fe-add7-6b82e22fcdb6"
-            target="_blank"
-          >
-            lighthouse score
-          </Link>
-          .
-        </Text>
-
-        <Text className="italic font-bold">
-          Note: Having an ad blocker will break this demo.
-        </Text>
+       
       </section>
 
       <hr className="border-t border-accents-2 my-6" />
